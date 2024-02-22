c1bfc82b38524c7f23878859bf29906970744bcf
J-Walega <kubawalega1@gmail.com>
2024-02-22 16:22:33 +0100

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -12,6 +12,7 @@ import { execSync } from 'child_process'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: 'https://j-walega.github.io/BibliothecaClient/',
   plugins: [
     vue({
       template: { transformAssetUrls }
